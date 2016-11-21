92377573976e0b8b82389a1a3ca762140a7c4d5b
mwgyn <ceo@ayampercik.com>
2016-11-21 17:31:22 +0800

    Added files via upload.  Initial

---
+++ WindowsApplication3/Form1.Designer.vb
@@ -0,0 +1,206 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnGo = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.statusLabel = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.trackBarBrightness = New System.Windows.Forms.TrackBar()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.btnBLCRefresh = New System.Windows.Forms.Button()
+        Me.btnDisableBLC = New System.Windows.Forms.Button()
+        Me.trackBarContrast = New System.Windows.Forms.TrackBar()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.trackBarGamma = New System.Windows.Forms.TrackBar()
+        Me.Label4 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.trackBarBrightness, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.trackBarContrast, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.trackBarGamma, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnGo
+        '
+        Me.btnGo.Location = New System.Drawing.Point(31, 25)
+        Me.btnGo.Name = "btnGo"
+        Me.btnGo.Size = New System.Drawing.Size(75, 23)
+        Me.btnGo.TabIndex = 0
+        Me.btnGo.Text = "GO"
+        Me.btnGo.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(31, 205)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(608, 358)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(525, 5)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(64, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "How to use:"
+        '
+        'statusLabel
+        '
+        Me.statusLabel.AutoSize = True
+        Me.statusLabel.Location = New System.Drawing.Point(28, 98)
+        Me.statusLabel.Name = "statusLabel"
+        Me.statusLabel.Size = New System.Drawing.Size(58, 13)
+        Me.statusLabel.TabIndex = 4
+        Me.statusLabel.Text = "Status : Nil"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(172, 5)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(56, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Brightness"
+        '
+        'trackBarBrightness
+        '
+        Me.trackBarBrightness.Location = New System.Drawing.Point(175, 25)
+        Me.trackBarBrightness.Minimum = 1
+        Me.trackBarBrightness.Name = "trackBarBrightness"
+        Me.trackBarBrightness.Size = New System.Drawing.Size(159, 45)
+        Me.trackBarBrightness.TabIndex = 6
+        Me.trackBarBrightness.Value = 1
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(528, 21)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(220, 104)
+        Me.TextBox1.TabIndex = 7
+        Me.TextBox1.Text = "Press Go and select the Video window and cover the lens for 3 seconds." & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Press """ &
+    "BLC Refresh"" to update BLC Mask" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Press ""Disable BLC"" to remove BLC Mask"
+        '
+        'btnBLCRefresh
+        '
+        Me.btnBLCRefresh.Location = New System.Drawing.Point(402, 25)
+        Me.btnBLCRefresh.Name = "btnBLCRefresh"
+        Me.btnBLCRefresh.Size = New System.Drawing.Size(97, 23)
+        Me.btnBLCRefresh.TabIndex = 8
+        Me.btnBLCRefresh.Text = "BLC Refresh"
+        Me.btnBLCRefresh.UseVisualStyleBackColor = True
+        '
+        'btnDisableBLC
+        '
+        Me.btnDisableBLC.Location = New System.Drawing.Point(404, 67)
+        Me.btnDisableBLC.Name = "btnDisableBLC"
+        Me.btnDisableBLC.Size = New System.Drawing.Size(95, 23)
+        Me.btnDisableBLC.TabIndex = 9
+        Me.btnDisableBLC.Text = "Disable BLC"
+        Me.btnDisableBLC.UseVisualStyleBackColor = True
+        '
+        'trackBarContrast
+        '
+        Me.trackBarContrast.Location = New System.Drawing.Point(175, 80)
+        Me.trackBarContrast.Maximum = 200
+        Me.trackBarContrast.Minimum = 1
+        Me.trackBarContrast.Name = "trackBarContrast"
+        Me.trackBarContrast.Size = New System.Drawing.Size(159, 45)
+        Me.trackBarContrast.TabIndex = 10
+        Me.trackBarContrast.Value = 110
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(172, 67)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(46, 13)
+        Me.Label3.TabIndex = 11
+        Me.Label3.Text = "Contrast"
+        '
+        'trackBarGamma
+        '
+        Me.trackBarGamma.Location = New System.Drawing.Point(175, 144)
+        Me.trackBarGamma.Maximum = 255
+        Me.trackBarGamma.Minimum = 1
+        Me.trackBarGamma.Name = "trackBarGamma"
+        Me.trackBarGamma.Size = New System.Drawing.Size(159, 45)
+        Me.trackBarGamma.TabIndex = 12
+        Me.trackBarGamma.Value = 127
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(175, 128)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(43, 13)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "Gamma"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(872, 509)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.trackBarGamma)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.trackBarContrast)
+        Me.Controls.Add(Me.btnDisableBLC)
+        Me.Controls.Add(Me.btnBLCRefresh)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.trackBarBrightness)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.statusLabel)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.btnGo)
+        Me.Name = "Form1"
+        Me.Text = "BLCViewerRefresh"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.trackBarBrightness, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.trackBarContrast, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.trackBarGamma, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnGo As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents statusLabel As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents trackBarBrightness As TrackBar
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents btnBLCRefresh As Button
+    Friend WithEvents btnDisableBLC As Button
+    Friend WithEvents trackBarContrast As TrackBar
+    Friend WithEvents Label3 As Label
+    Friend WithEvents trackBarGamma As TrackBar
+    Friend WithEvents Label4 As Label
+End Class
---
+++ WindowsApplication3/Form1.vb
@@ -0,0 +1,290 @@
+﻿Imports System
+Imports System.Drawing
+Imports System.Drawing.Drawing2D
+Imports System.Runtime.InteropServices
+Imports System.Drawing.Imaging
+Imports System.IO
+Imports System.Text
+
+Public Class Form1
+    Dim bmpBLCMask As Bitmap
+    Dim windowID As Integer
+    Dim finalPicture As Bitmap
+
+
+
+
+    ''  All the stuff related to Window capture below
+
+    Private WithEvents Tmr As New Timer With {.Interval = 1}
+
+    <DllImport("user32.dll", EntryPoint:="GetWindowRect")>
+    Private Shared Function GetWindowRect(ByVal hWnd As System.IntPtr, ByRef lpRect As RECT) As <MarshalAs(UnmanagedType.Bool)> Boolean
+    End Function
+
+    <DllImport("user32.dll", EntryPoint:="GetForegroundWindow")>
+    Private Shared Function GetForegroundWindow() As Integer
+    End Function
+
+    <StructLayout(LayoutKind.Sequential)>
+    Private Structure RECT
+        Public left As Integer
+        Public top As Integer
+        Public right As Integer
+        Public bottom As Integer
+    End Structure
+
+    'Private Sub Tmr_Tick(ByVal sender As Object, ByVal e As System.EventArgs) Handles Tmr.Tick
+    '    Tmr.Stop()
+    '    If PictureBox1.Image IsNot Nothing Then PictureBox1.Image.Dispose()
+    '    PictureBox1.SizeMode = PictureBoxSizeMode.AutoSize
+    '    PictureBox1.Image = GetForgroundWindowImage()
+    '    resetBLCMask()
+    '    Me.Show()
+
+    'End Sub
+
+    Private Function GetForgroundWindowImage() As Image
+        Dim bmp As Bitmap = Nothing
+        'Dim hWnd As IntPtr = GetForegroundWindow()
+        Dim hWnd As Integer = windowID  ' fix the damn window
+        If Not hWnd.Equals(IntPtr.Zero) Then
+            Dim rec As New RECT
+            GetWindowRect(hWnd, rec)
+            Dim recSize As New Size(rec.right - rec.left, rec.bottom - rec.top)
+            bmp = New Bitmap(recSize.Width, recSize.Height)
+            Dim grfx As Graphics = Graphics.FromImage(bmp)
+            grfx.CopyFromScreen(rec.left, rec.top, 0, 0, recSize, CopyPixelOperation.SourceCopy)
+            grfx.Dispose()
+        End If
+        Return bmp
+    End Function
+
+
+    ''  All the stuff related to Window capture above
+
+    ''
+    ''
+    ''
+    ''
+    ''
+    ''
+    '' What happens when GO is clicked :
+
+    Private Sub btnGo_Click(sender As Object, e As EventArgs) Handles btnGo.Click
+        statusLabel.Text = "Status : Cover lens now"
+        Refresh()
+        Threading.Thread.Sleep(3000)
+        windowID = GetForegroundWindow()
+        'If bmpBLCMask Is Nothing Then
+        'Dim result As Integer = MessageBox.Show("Cover lens and press OK", "BLC Calibration", MessageBoxButtons.OK)
+        'If result = DialogResult.OK Then
+        resetBLCMask()
+        '    End If
+        'End If
+
+        statusLabel.Text = "Status : Ok you can let go now"
+        Refresh()
+        'Threading.Thread.Sleep(3000) ' Wait 1 second
+
+        subtractImage()
+        Me.Refresh()
+        'loopLikeMad()
+        statusLabel.Text = "Status : Live streaming"
+        Tmr.Start()
+
+
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As Object, ByVal e As System.EventArgs) Handles Tmr.Tick
+        'subtractImage()
+
+        Me.Refresh()
+        If PictureBox1.Image IsNot Nothing Then PictureBox1.Image.Dispose()
+        subtractImage()
+
+    End Sub
+
+
+
+    'Private Sub loopLikeMad()
+    '    For index As Integer = 1 To 5
+    '        'subtractImage()
+    '        statusLabel.Text = index.ToString
+    '        'Me.Refresh()
+    '        statusLabel.Refresh()
+    '        Threading.Thread.Sleep(30)
+    '    Next
+
+    'End Sub
+
+    Private Sub resetBLCMask()
+        bmpBLCMask = GetForgroundWindowImage()
+        'bmpgrBLCMask = Graphics.FromImage(bmpBLCMask)
+
+    End Sub
+
+    Private Sub removeBLCMask()
+        Dim blackMask As Bitmap = bmpBLCMask
+        Dim wid As Integer = blackMask.Width
+        Dim hgt As Integer = blackMask.Height
+
+        'Dim r1, g1, b1, As Integer
+        Dim color1 As Color
+
+        For x As Integer = 0 To wid - 1
+            For y As Integer = 0 To hgt - 1
+                'color1 = blackMask.GetPixel(x, y)
+                ''r1 = color1.R : g1 = color1.G : b1 = color1.B
+
+                color1 = Color.FromArgb(255, 0, 0, 0)
+                blackMask.SetPixel(x, y, color1)
+            Next y
+        Next x
+
+        bmpBLCMask = blackMask
+
+
+
+    End Sub
+
+    Private Sub subtractImage()
+
+
+        ' Load the images.
+        Dim bm1 As Bitmap = bmpBLCMask
+        Dim bm2 As Bitmap = GetForgroundWindowImage()
+
+
+        ' Make a difference image.
+        'Dim wid As Integer = Math.Min(bm1.Width, bm2.Width)
+        'Dim hgt As Integer = Math.Min(bm1.Height, bm2.Height)
+        Dim wid As Integer = bm1.Width
+        Dim hgt As Integer = bm1.Height
+        Dim bm3 As New Bitmap(wid, hgt)
+
+
+        Dim are_identical As Boolean = True
+        Dim r1, g1, b1, r2, g2, b2, r3, g3, b3 As Integer
+        Dim color1, color2, color3 As Color
+        'get the image width and length
+        For x As Integer = 0 To wid - 1
+            For y As Integer = 0 To hgt - 1
+                color1 = bm1.GetPixel(x, y)
+                r1 = color1.R : g1 = color1.G : b1 = color1.B
+
+                color2 = bm2.GetPixel(x, y)
+                r2 = color2.R : g2 = color2.G : b2 = color2.B
+
+                'r3 = 128 + (r1 - r2) \ 2
+                'g3 = 128 + (g1 - g2) \ 2
+                'b3 = 128 + (b1 - b2) \ 2
+
+                r3 = Math.Abs(r2 - r1)
+                g3 = Math.Abs(g2 - g1)
+                b3 = Math.Abs(b2 - b1)
+
+                r3 = Math.Min(r3 * trackBarBrightness.Value, 255)
+                g3 = Math.Min(g3 * trackBarBrightness.Value, 255)
+                b3 = Math.Min(b3 * trackBarBrightness.Value, 255)
+
+                If r3 < trackBarGamma.Value Then
+                    r3 = Math.Max(Math.Round(r3 * 0.01 * trackBarContrast.Value), 0)
+                Else
+                    r3 = Math.Min(Math.Round(r3 * 0.01 * (200 - trackBarContrast.Value)), 255)
+                End If
+
+                If g3 < trackBarGamma.Value Then
+                    g3 = Math.Max(Math.Round(g3 * 0.01 * trackBarContrast.Value), 0)
+                Else
+                    g3 = Math.Min(Math.Round(g3 * 0.01 * (200 - trackBarContrast.Value)), 255)
+                End If
+
+                If b3 < trackBarGamma.Value Then
+                    b3 = Math.Max(Math.Round(b3 * 0.01 * trackBarContrast.Value), 0)
+                Else
+                    b3 = Math.Min(Math.Round(b3 * 0.01 * (200 - trackBarContrast.Value)), 255)
+                End If
+
+
+                color3 = Color.FromArgb(255, r3, g3, b3)
+                bm3.SetPixel(x, y, color3)
+
+                If (r1 <> r2) OrElse (g1 <> g2) OrElse (b1 <>
+                b2) Then are_identical = False
+            Next y
+        Next x
+
+        finalPicture = bm3
+        'Display the result.
+        PictureBox1.SizeMode = PictureBoxSizeMode.AutoSize
+        PictureBox1.Image = finalPicture
+        'PictureBox1.Refresh()
+
+        'finalPicture = bm3
+        'bm1.Dispose()
+        bm2.Dispose()
+        'bm3.Dispose()
+
+    End Sub
+
+    Private Sub trackBarBrightness_Scroll(sender As Object, e As EventArgs) Handles trackBarBrightness.Scroll
+        Label2.Text = String.Format("Brightness: {0}", trackBarBrightness.Value)
+    End Sub
+
+    'Private Sub frmScreenCopy_KeyDown(sender As Object, e As System.Windows.Forms.KeyEventArgs) Handles Me.KeyDown
+    '    Select Case e.KeyCode
+    '        Case Keys.Escape
+    '            removeBLCMask()
+    '        'Case Keys.F2
+    '        '    If e.Control = False Then
+    '        '        Clipboard.SetImage(Desktop)
+    '        '    Else
+    '        '        ' make a copy inluding the glass
+    '        '        Dim bmp As Bitmap = pic.Image.Clone
+    '        '        Dim bmpgr As Graphics = Graphics.FromImage(bmp)
+    '        '        bmpgr.Clip = rgn
+    '        '        bmpgr.DrawImage(bmpOriCopy, rctMagn, rctOriCopy, GraphicsUnit.Pixel)
+    '        '        bmpgr.DrawEllipse(pn, rctMagn)
+    '        '        Clipboard.SetImage(bmp.Clone)
+    '        '        bmpgr.Dispose()
+    '        '        bmp.Dispose()
+    '        '    End If
+    '        'Case Keys.F4
+    '        '    If Cursor.Tag = "off" Then
+    '        '        Cursor.Show()
+    '        '        Cursor.Tag = "on"
+    '        '    Else
+    '        '        Cursor.Hide()
+    '        '        Cursor.Tag = "off"
+    '        '    End If
+    '        Case Keys.F5
+    '            PictureBox1.Hide()
+    '            'resetBLCMask()
+
+
+    '    End Select
+    'End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnBLCRefresh.Click
+        resetBLCMask()
+    End Sub
+
+    Private Sub btnDisableBLC_Click(sender As Object, e As EventArgs) Handles btnDisableBLC.Click
+        removeBLCMask()
+    End Sub
+
+    Private Sub trackBarContrast_Scroll(sender As Object, e As EventArgs) Handles trackBarContrast.Scroll
+        Label3.Text = String.Format("Contrast: {0}", trackBarContrast.Value)
+    End Sub
+
+
+
+    Private Sub TrackBar1_Scroll(sender As Object, e As EventArgs) Handles trackBarGamma.Scroll
+        Label4.Text = String.Format("Gamma: {0}", trackBarGamma.Value)
+    End Sub
+End Class
